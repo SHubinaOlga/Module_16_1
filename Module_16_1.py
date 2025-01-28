17256c9a8c2613df76a4de0a0244a0b4dc0e2913
SHubinaOlga <olga.shubina85@yandex.ru>
2025-01-28 22:25:27 +0300

    Module_16_1.py

--- Module_16_1.py
+++ Module_16_1.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def Get_Main_Page():
+async def Get_Main_Page() -> str:
     return "Главная страница"
 
 @app.get("/user/admin")
-async def Get_Admin_Page():
+async def Get_Admin_Page() -> str:
     return "Вы вошли как администратор"
 
 @app.get("/users/{user_id}")
-async def Get_User_id(user_id: int):
+async def Get_User_id(user_id: int) -> str:
     return f"Вы вошли как пользователь № {user_id}"
 
 @app.get("/user")
-async def Get_User_Info(username: str = 'Olga', age: int= 23):
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+async def Get_User_Info(username: str = 'Olga', age: int= 23) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
